5599922e3b09972f5eed80e0d7ddd4e58b64a9a1
Alexis Gallèpe <alexis.gallepe@gmail.com>
2016-09-27 17:44:56 +0200

    done exercice 6

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def nodes_degrees(graph):
 
     for edge in graph:
         nodes = get_nodes_from_edge(edge)
+        print(nodes)
         n0 = nodes[0]
 
         if len(nodes) == 1:
@@ -48,7 +49,7 @@ def load_graph(graph):
         if len(ns) == 1:
             nodes[n0] = []
 
-        # if nodes already in disct, append his new neighbour
+        # if nodes already in dict, append his new neighbour
         elif n0 in nodes:
             nodes[n0].append(ns[1])
 
@@ -86,16 +87,33 @@ def get_max_degree_nodes(loadedGraph):
     return len( max(loadedGraph.values()) )
 
 
+# Exercice 6
+def degree_distribution(loadedGraph):
+    distribution = {}
+    max_degree = get_max_degree_nodes(loadedGraph)
+
+    for i in range(max_degree+1):
+        distribution[i] = 0
+
+    for n in loadedGraph:
+        distribution[len(loadedGraph[n])]+=1
+
+    return distribution
+
+
+
 if "__main__" == __name__:
 
-    with open("graph2", "r+") as file:
+    with open("graph", "r+") as file:
         graph= file.read().splitlines()
+        print(graph)
 
         print("Exercise n°2, size of graph: ", size_of_graph(graph))
         print("Exercise n°3, degree of each nodes:", nodes_degrees(graph))
 
-
         ld = load_graph(graph)
+
+        print("Exercise n°4, loaded graph:", ld)
 
         print("\nExercise n°5:")
         print("\tNumber of nodes of degree 0: ",get_zero_degree_nodes(ld))
@@ -103,3 +121,6 @@ if "__main__" == __name__:
         print("\tAverage degre of the graph: ")
         print("\tMinimum degree of the graph: ",get_min_degree_nodes(ld))
         print("\tMaximum degree of the graph: ",get_max_degree_nodes(ld))
+
+
+        print("\nExercice 6: ", degree_distribution(ld))
